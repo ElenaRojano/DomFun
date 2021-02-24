9fcdf16042cb584a7e8438cf8d2709bb441d4a71
elenarojano <elenarojano@uma.es>
2021-02-24 12:12:02 +0100

    Updates in code to prepare normalized predictions files for CAFA 3 assessment

--- bin/predictions2CAFA3_format.rb
+++ bin/predictions2CAFA3_format.rb
@@ -3,6 +3,9 @@
 ##########################
 # Rojano E. & Seoane P., Feb 2021
 # Generate files for CAFA 3 validation (from normalized predictions)
+# Load several files with normalized predictions and for each one returns:
+# 1. A yaml file for each predictions file included with info for CAFA 3 validation
+# 2. A yaml file with info for all predictions to plot CAFA 3 validation graphs
 ##########################
 
 REPORT_FOLDER=File.expand_path(File.join(File.dirname(__FILE__), '..', 'templates'))
@@ -10,6 +13,8 @@ ROOT_PATH = File.dirname(__FILE__)
 $: << File.expand_path(File.join(ROOT_PATH, '..', 'lib', 'DomFun'))
 require 'generalMethods'
 require 'optparse'
+require 'yaml'
+require 'fileutils'
 #require 'csv'
 
 ##########################
@@ -17,21 +22,25 @@ require 'optparse'
 #########################
 # Corregir el input de combined scores
 
-def load_predictions(input_file)
-	predictions = {}
-	File.open(input_file).each do |line|
-		line.chomp! 
-    proteinID, domains, go_term, p_value = line.split("\t")	
-    #STDERR.puts p_value.inspect
-    p_value = sprintf "%.2f", p_value.to_f
-    query = predictions[proteinID]
-    if query.nil?
-      predictions[proteinID] = [[go_term, p_value]]
-    else
-      query << [go_term, p_value]
-    end 
+def load_predictions(prediction_files)
+	all_predictions = {}
+  predictions = {}
+	prediction_files.each do |f|
+    filename = File.basename(f).split(".").first
+    File.open(f).each do |line|
+  		line.chomp! 
+      proteinID, domains, go_term, p_value = line.split("\t")	
+      p_value = sprintf "%.2f", p_value.to_f
+      query = predictions[proteinID]
+      if query.nil?
+        predictions[proteinID] = [[go_term, p_value]]
+      else
+        query << [go_term, p_value]
+      end 
+    end
+    all_predictions[filename] = predictions #SAVE ALL HASHES FOR EACH PREDICTION FILE
   end
-  return predictions
+  return all_predictions
 end
 
 def translate_uniprot_to_CAFA(predictions, cath_dict, cafa_dict)
@@ -53,6 +62,47 @@ def translate_uniprot_to_CAFA(predictions, cath_dict, cafa_dict)
 	return cafa3_predictions, untranslated_proteins
 end
 
+def generate_yaml_hash_predictions(output_file_path, obo_path, benchmark_files, yaml_output)
+  yaml_hash = {
+    "assess" => {
+      "file" => output_file_path, "obo" => obo_path, "benchmark" => benchmark_files, "results" => yaml_output
+    }
+  }
+  return yaml_hash
+end
+
+def generate_plot_yaml(plot_results_path, title, smooth, prediction_files)
+  filenames = prediction_files.map do |f|
+    File.basename(f).split(".").first
+  end
+  yaml_hash = {
+    "plot" => {
+      "results" => plot_results_path, "title" => title, "smooth" => smooth
+    }
+  }
+  counter = 1
+  filenames.each do |filename|
+    yaml_hash["plot"]["file" + counter.to_s] = filename
+    counter += 1
+  end
+  return yaml_hash
+
+end
+
+def generate_output_path(domain_class, association_method, string, file_path)
+  output_name = []
+  output_name << domain_class
+  output_name << association_method
+  output_name << string
+  filename = output_name.join('_')
+  full_output_path = File.join(file_path, filename)
+  return full_output_path, filename
+end
+
+# , 
+#       "plot" => {"results" => plot_results_path, "title" => title, "smooth" => smooth, "file1" => output_file1
+#     }
+
 ##########################
 #OPT-PARSER
 ##########################
@@ -61,33 +111,63 @@ OptionParser.new do |opts|
   opts.banner = "Usage: #{__FILE__} [options]"
 
   options[:input_predictions] = nil
-  opts.on("-a", "--input_predictions PATH", "Input file with normalized predictions") do |input_predictions|
+  opts.on("-a", "--input_predictions PATH", "Input files with normalized predictions") do |input_predictions|
     options[:input_predictions] = input_predictions
   end
 
-  options[:cath_dict] = nil
-  opts.on("-c", "--cath_dict PATH", "Input CATH file to translate UniProtIDs to GeneIDs") do |data|
-    options[:cath_dict] = data
+  options[:benchmark_path] = nil
+  opts.on("-b", "--benchmark_path PATH", "Input PATH with benchmark files for CAFA 3") do |data|
+    options[:benchmark_path] = data
+  end  
+
+  options[:accesion_geneid_dictionary] = nil
+  opts.on("-c", "--accesion_geneid_dictionary PATH", "Input UniProt accesion-GeneID dictionary file to translate UniProtIDs to GeneIDs") do |data|
+    options[:accesion_geneid_dictionary] = data
   end
 
-  options[:cafa_dict] = nil
-  opts.on("-d", "--cafa_dict PATH", "Input CAFA3 file to translate GeneIDs to CAFA3IDs") do |data|
-    options[:cafa_dict] = data
+  options[:targetID_geneid_dictionary] = nil
+  opts.on("-d", "--targetID_geneid_dictionary PATH", "Input file to translate GeneIDs to CAFA3 IDs") do |data|
+    options[:targetID_geneid_dictionary] = data
   end  
 
   options[:domains_class] = 'funfamID'
-  opts.on("-D", "--domains_class STRING", "Domains type") do |data|
+  opts.on("-e", "--domains_class STRING", "Domains type to use as group name in yaml file") do |data|
     options[:domains_class] = data
   end  
 
-  options[:output_file] = 'results_to_CAFA3_validation.txt'
-  opts.on("-o", "--output_file PATH", "Output file") do |output_file|
-    options[:output_file] = output_file
+  options[:association_method] = 'hypergeometric'
+  opts.on("-f", "--association_method STRING", "Association method to use as mode in yaml file") do |data|
+    options[:association_method] = data
+  end  
+
+  options[:cafa3_results] = 'CAFA3_results'
+  opts.on("-g", "--cafa3_results PATH", "Path to save CAFA 3 plot files") do |data|
+    options[:cafa3_results] = data
   end
 
-  options[:untranslated_proteins] = 'untranslated_proteins.txt'
-  opts.on("-u", "--untranslated_proteins PATH", "Output file with untranslated proteins list") do |data|
+  options[:output_path] = nil
+  opts.on("-o", "--output_path PATH", "Output file") do |data|
+    options[:output_path] = data
+  end
+
+  options[:obo_path] = nil
+  opts.on("-p", "--obo_path PATH", "OBO file for CAFA 3 analysis") do |data|
+    options[:obo_path] = data
+  end
+
+  options[:untranslated_proteins] = nil
+  opts.on("-u", "--untranslated_proteins PATH", "Output path with untranslated proteins list") do |data|
     options[:untranslated_proteins] = data
+  end
+
+  options[:yaml_file] = nil
+  opts.on("-y", "--yaml_file PATH", "Output path to storage yaml files for CAFA 3 validation") do |data|
+    options[:yaml_file] = data
+  end
+
+  opts.on_tail("-h", "--help", "Show this message") do
+    puts opts
+    exit
   end
 
 end.parse!
@@ -95,22 +175,63 @@ end.parse!
 ##########################
 #MAIN
 ##########################
+prediction_files = Dir.glob(options[:input_predictions])
+all_predictions = load_predictions(prediction_files)
+accesion_geneid_dictionary = load_hash(options[:accesion_geneid_dictionary], 'a')
+targetID_geneid_dictionary = load_hash(options[:targetID_geneid_dictionary], 'b')
 
-predictions = load_predictions(options[:input_predictions])
-cath_dict = load_hash(options[:cath_dict], 'a')
-cafa_dict = load_hash(options[:cafa_dict], 'b')
-cafa3_predictions, untranslated_proteins = translate_uniprot_to_CAFA(predictions, cath_dict, cafa_dict)
-#header = "AUTHOR Bio267\nMODEL 1\nKEYWORDS sequence alignment.\n"
-#footer = "END"
-File.open(options[:output_file], 'w') do |f|
-  #f.puts header
-  cafa3_predictions.each do |cafaID, predictions|
-  	predictions.each do |prediction|
-  		f.puts "#{cafaID}\t#{prediction.join("\t")}"
-  	end
+# OUTPUT PREDICTION FILE FORMAT HEADER AND FOOTER:
+header = "AUTHOR #{options[:domains_class]}\nMODEL #{options[:association_method]}\nKEYWORDS sequence alignment.\n"
+footer = "END"
+
+# YAML FILES PARAMETERS:
+obo_path = options[:obo_path]
+benchmark_files = options[:benchmark_path]
+title = "all"
+smooth = "N"
+
+domain_class = nil
+association_method = nil
+
+all_prediction_filenames = []
+# REFORMED CAFA 3 PREDICTION FILES:
+all_predictions.each do |filename, prediction|
+  cafa3_predictions, untranslated_proteins = translate_uniprot_to_CAFA(prediction, accesion_geneid_dictionary, targetID_geneid_dictionary)
+  domain_class = filename.split('_')[1]
+  association_method = filename.split('_')[2]
+  full_output_path, filename = generate_output_path(domain_class, association_method, 'all.txt', options[:output_path])
+  all_prediction_filenames << filename
+
+  File.open(full_output_path, 'w') do |f|
+    f.puts header
+    cafa3_predictions.each do |cafaID, predictions|
+    	predictions.each do |prediction|
+    		f.puts "#{cafaID}\t#{prediction.join("\t")}"
+    	end
+    end
+    f.puts footer
   end
-  #f.puts footer
+
+  # GENERATE YAML FILES FOR PREDICTIONS:
+  output_file_path = full_output_path #where CAFA 3 predictions file is stored
+  full_output_path, filename = generate_output_path(domain_class, association_method, 'config_launch.yaml', options[:yaml_file])
+  yaml_output = full_output_path 
+  output_file1 = [options[:domains_class], options[:association_method], title].join('_')
+
+  FileUtils.mkdir_p(options[:cafa3_results])
+
+  yaml_pred_hash = generate_yaml_hash_predictions(output_file_path, obo_path, benchmark_files, yaml_output)
+  File.open(full_output_path, "w") { |file| file.write(yaml_pred_hash.to_yaml) }
+
+  # GENERATE FILE WITH LOST PROTEINS:
+  full_output_path, filename = generate_output_path(domain_class, association_method, 'unnanotated_proteins.txt', options[:untranslated_proteins])
+  File.open(full_output_path, 'w') do |f|
+    f.puts untranslated_proteins.join("\n")
+  end
+
 end
-File.open(options[:untranslated_proteins], 'w') do |f|
-  f.puts untranslated_proteins.join("\n")
-end
+
+#Generate uniq PLOT file
+yaml_plot_hash = generate_plot_yaml(options[:cafa3_results], title, smooth, all_prediction_filenames)
+yaml_plot_all_filename = File.join(options[:yaml_file], 'plot_all.yaml')
+File.open(yaml_plot_all_filename, "w") { |file| file.write(yaml_plot_hash.to_yaml) }
